150c111d96f42a4202c0a2c74bd1b767562b5b92
azakimi123 <aaron.zakimi@gmail.com>
2020-07-09 13:51:56 -0600

    Practice Joins complete

--- project.sql
+++ project.sql
@@ -1,5 +1,5 @@
 
---PRACTICE JOINS
+--PRACTICE JOINS--
 -- 1. Get all invoices where the unit_price on the invoice_line is greater than $0.99.
 SELECT * FROM invoice
 JOIN invoice_line ON invoice.invoice_id = invoice_line.invoice_id 
